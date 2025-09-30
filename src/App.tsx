6cca369f4027e2452247d705d7d5fb752773db52
Adi <ripedown1@gmail.com>
2025-09-30 15:43:34 +0530

    make design responsive

--- src/App.tsx
+++ src/App.tsx
@@ -5,52 +5,56 @@ import Header from "./components/Header";
 
 function App() {
 	return (
-		<div className="bg-black w-screen h-screen text-white flex relative">
+		<div className="bg-black w-screen h-screen text-white flex flex-col relative overflow-hidden">
 			<Header />
-			<div
-				className="
-							absolute
-							inset-0
-							z-20
-							flex
-							flex-col
-							justify-center
-							items-center
-						"
-			>
-				<h1
-					className="text-8xl font-extrabold italic tracking-wider"
-					style={{
-						alignSelf: "flex-start",
-						marginLeft: "20%",
-						marginBottom: "5px",
-						// Added a distinct offset shadow
-						textShadow: "8px 8px 0px rgba(0, 0, 0, 0.2)",
-					}}
-				>
-					RENDERLY
-				</h1>
-				<h1
-					// Removed inline styles and added a class for the hollow effect
-					className="text-8xl font-extrabold italic tracking-wider hollow-text"
-					data-text="STUDIO"
-					style={{
-						alignSelf: "flex-end",
-						marginRight: "20%",
-					}}
-				>
-					STUDIO
-				</h1>
-			</div>
 
-			<div className="flex-1  py-15 px-15 flex justify-start">
-				<img src={image1} alt="text" className="w-auto h-2/3 " />
-			</div>
-			<div className="flex-1  py-15 px-5 flex justify-center items-center">
-				<img src={image2} alt="text" className="w-auto h-2/3" />
-			</div>
-			<div className="flex-1  py-15 px-15 flex justify-end items-end">
-				<img src={image3} alt="text" className="w-auto h-2/3" />
+			{/* Main content area below header */}
+			<div className="flex-1 flex relative">
+				{/* Heading overlay */}
+				<div className="absolute inset-0 z-20 flex flex-col justify-center items-stretch px-4 pointer-events-none">
+					<h1
+						className="text-5xl sm:text-6xl md:text-6xl lg:text-7xl xl:text-8xl font-black italic tracking-wider heading-shadow self-start md:ml-[20%]"
+						style={{
+							marginBottom: "5px",
+						}}
+					>
+						RENDERLY
+					</h1>
+					<h1
+						className="text-5xl sm:text-6xl md:text-6xl lg:text-7xl xl:text-8xl font-black italic tracking-wider hollow-text self-end md:mr-[20%]"
+						data-text="STUDIO"
+					>
+						STUDIO
+					</h1>
+				</div>
+
+				{/* Desktop: 3 images side by side */}
+				<div className="hidden md:flex flex-1 py-15 px-15 justify-start">
+					<img src={image1} alt="text" className="w-auto h-2/3 object-contain" />
+				</div>
+				<div className="hidden md:flex flex-1 py-15 px-5 justify-center items-center">
+					<img src={image2} alt="text" className="w-auto h-2/3 object-contain" />
+				</div>
+				<div className="hidden md:flex flex-1 py-15 px-15 justify-end items-end">
+					<img src={image3} alt="text" className="w-auto h-2/3 object-contain" />
+				</div>
+
+				{/* Mobile: center image with corner peeks */}
+				<div className="md:hidden flex-1 flex justify-center items-center px-5">
+					<img src={image2} alt="text" className="w-auto max-h-[60vh] object-contain" />
+				</div>
+
+				{/* Mobile: corner images */}
+				<img
+					src={image1}
+					alt="text"
+					className="md:hidden absolute top-0 left-0 w-40 h-40 sm:w-48 sm:h-48 object-cover object-bottom-right -translate-x-16 -translate-y-16 opacity-50 z-0"
+				/>
+				<img
+					src={image3}
+					alt="text"
+					className="md:hidden absolute bottom-0 right-0 w-40 h-40 sm:w-48 sm:h-48 object-cover object-top-left translate-x-16 translate-y-16 opacity-50 z-0"
+				/>
 			</div>
 		</div>
 	);
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,12 +2,11 @@ import React from "react";
 
 const Header: React.FC = () => {
 	return (
-		<header className="absolute top-0 left-0 w-full p-4 z-30">
+		<header className="w-full p-4 bg-black z-30">
 			<nav className="flex justify-center items-center">
 				<p className="tracking-widest text-sm font-light text-gray-300">COMING SOON</p>
 			</nav>
 		</header>
 	);
 };
-
 export default Header;
